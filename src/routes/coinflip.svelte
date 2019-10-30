db6e3db170955c8a9937c4123e5159401d668971
Paul Karayan <paulkarayan@gmail.com>
2019-10-29 21:37:44 -0700

    works

--- src/routes/coinflip.svelte
+++ src/routes/coinflip.svelte
@@ -1,13 +1,14 @@
 <script>
   import Slider from "components/Slider";
-  $: value = 5;
   
   let heads = 0 
   let tails = 0 
 
-  function getFlip(value) {
-    console.log(value, "<----")
-    fetch("http://localhost:5000/coinflips/34")
+  let val = 5
+
+  function getFlip() {
+    console.log(val, "<----")
+    fetch("http://localhost:5000/coinflips/"+val)
       .then(response => {
         return response.json()
       })
@@ -29,7 +30,12 @@
   Heads {heads}
   Tails {tails}
   <h4 class="my-8">Coin Flip Simulator</h4>
-  <caption>Number of coin flips: {value}</caption>
-  <Slider min="1" max="100" bind:value on:change = {getFlip} />
+  <!-- <caption>Number of coin flips: {val}</caption> -->
+  <!-- <Slider min="1" max="100" bind:value={val} on:change={getFlip(val)} /> -->
+  
+  <caption>Number of coin flips:</caption>
+  <input type=int bind:value={val}>
+  <button on:click={getFlip}>Submit</button>
+
 
 </div>

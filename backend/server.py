8ef8dddd8aa2a65a374fd2154244cba53b18482d
Paul Karayan <paulkarayan@gmail.com>
2019-10-29 20:11:10 -0700

    coinflips api

--- backend/server.py
+++ backend/server.py
@@ -10,9 +10,21 @@ def random_number():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route("/rand")
-def random_number():
-    response = jsonify({'data': str(random.randint(0, 100))})
+@app.route("/coinflips/<int:nums>")
+def coin_flipper(nums):
+    # naive way
+    heads, tails = 0, 0 
+    for idx in range(nums):
+        if random.randint(0, 1):
+            heads += 1
+        else:
+            tails += 1
+
+    # or lets do it even easier
+    heads = random.randint(0, nums)
+    response = jsonify({'heads': heads,
+                        'tails': nums - heads,
+                        })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
